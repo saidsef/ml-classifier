a27cdcb0185cee3599168f5eb364233011f796e8
Said Sef <saidsef@gmail.com>
2018-06-29 07:28:45 +0000

    added logging to news-ml

--- news-ml.py
+++ news-ml.py
@@ -2,6 +2,7 @@
 
 import os
 import pickle
+import logging
 import numpy as np
 from json import loads, dumps
 from flask import Flask, request, jsonify
@@ -9,6 +10,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import make_pipeline
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 PORT = os.environ.get("PORT")
 pkl  = open('./data/lsvc.pickle', 'rb')
