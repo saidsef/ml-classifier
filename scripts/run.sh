69459cc9395f818579b2b9bf767aebb0e3e5e2de
Said Sef <6002187+saidsef@users.noreply.github.com>
2024-04-15 23:57:53 +0100

    fix: release run script requirements path (#153)

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ set -ex
 export TIMEOUT=${NOTEBOOK_TIMEOUT:-1200}
 
 function runnb() {
-  pip3 install $(cat ../requirements.txt | grep -i scikit-learn) -v
+  pip3 install $(cat ./requirements.txt | grep -i scikit-learn) -v
   time jupyter nbconvert --ExecutePreprocessor.timeout=$TIMEOUT --execute --to notebook --inplace classifier-ml.ipynb
 }
 
